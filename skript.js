ab79d86fc909d537ea7fa0338559bf8f18edc02b
XDsiroriginal <144023827+XDsiroriginal@users.noreply.github.com>
2024-06-08 11:44:06 +0300

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,7 +1,23 @@
 let id;
 let element;
+let loading;
+let first_color;
+let second_color;
+var buttonStyle;
+let buton;
+var backgroundImage;
+var colors;
 function color()
 {
+	loading = document.getElementById("loads");
+
 	element = event.target.id;
 	console.log(element);
+	buton = document.getElementById(element);
+
+	buttonStyle = window.getComputedStyle(buton);
+	backgroundImage = buttonStyle.backgroundImage;
+	console.log(backgroundImage);
+
+	loading.borderimage = backgroundImage;
 }
